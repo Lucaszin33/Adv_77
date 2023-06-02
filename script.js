d0a3d9873f4bbd0efc524f4a3cee73359edfdff6
Lucaszin33 <110205505+Lucaszin33@users.noreply.github.com>
2023-06-01 21:03:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Agora sim você entendeu o que eu disse né?")
+var primeiro_nome = "Cleiton";
+var segundo_nome = "Silva";
+console.log(primeiro_nome.concat(segundo_nome))
+var n1 = 10;
+var n2 = 5;
+var n3 = n1 + n2;
+console.log(n1 + n2 + " aqui eu utilizei duas variáveis de numeros e depois somei elas no console.log" )
+console.log(n3 + " aqui eu utilizei duas váriaveis de numeros e depois mais uma para soma-las o resultado está errado")
